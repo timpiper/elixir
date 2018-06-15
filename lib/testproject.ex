a7e62f42fe1d94caff6daccbebc6dbf6df1526a4
Timo Piper <t.piper@wertgarantie.com>
2018-06-15 13:33:39 +0200

    init

---
+++ lib/testproject.ex
@@ -0,0 +1,18 @@
+defmodule Testproject do
+  @moduledoc """
+  Documentation for Testproject.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Testproject.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
